789ba6a4d8ac8f44274e2b51ad07a09d56facc47
AmS <alimirjahani7@gmail.com>
2018-06-26 13:20:51 +0430

    delivery method made => now test

--- spec/model/delivery.spec.js
+++ spec/model/delivery.spec.js
@@ -8,29 +8,45 @@ const warehouses = require('../../warehouses');
 describe('making shelf code', () => {
   let deliveries;
   let hub_id;
+  let delivery_id;
   beforeEach(done => {
     lib.dbHelpers.dropAll().then(() => {
       return models['WarehouseTest'].insertMany(warehouses)
     }).then((res) => {
       hub_id = res.find(w => w.is_hub === true)._id;
-      deliveries = [{
-        order_details: {
-          order_id: mongoose.Types.ObjectId(),
-          order_line_ids: [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()]
-        },
-        from: {warehouse_id: hub_id},
-        to: {warehouse_id: mongoose.Types.ObjectId()},
-        shelf_code: "AB",
-      },
+      deliveries = [
         {
+          _id: mongoose.Types.ObjectId(),
           order_details: {
             order_id: mongoose.Types.ObjectId(),
             order_line_ids: [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()]
           },
-          from: {warehouse_id: mongoose.Types.ObjectId()},
-          to: {warehouse_id: hub_id},
-          shelf_code: "AB",
-        }];
+          from: {warehouse_id: hub_id},
+          to: {warehouse_id: mongoose.Types.ObjectId()},
+          shelf_code: "AZ",
+        },
+        {
+          _id: mongoose.Types.ObjectId(),
+          order_details: {
+            order_id: mongoose.Types.ObjectId(),
+            order_line_ids: [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()]
+          },
+          from: {warehouse_id: hub_id},
+          to: {warehouse_id: mongoose.Types.ObjectId()},
+          shelf_code: "AA",
+        },
+        {
+          _id: mongoose.Types.ObjectId(),
+          order_details: {
+            order_id: mongoose.Types.ObjectId(),
+            order_line_ids: [mongoose.Types.ObjectId(), mongoose.Types.ObjectId()]
+          },
+          from: {warehouse_id: hub_id},
+          to: {warehouse_id: mongoose.Types.ObjectId()},
+
+        }
+      ];
+      delivery_id = deliveries[2]._id;
       return models['DeliveryTest'].insertMany(deliveries)
     }).then(() => {
       done();
@@ -43,10 +59,9 @@ describe('making shelf code', () => {
   it('should make shelf code', function (done) {
     this.done = done;
     let delivery = new Delivery(true);
-    delivery.makeDeliveryShelfCode(5).then(data => {
+    delivery.makeDeliveryShelfCode(delivery_id).then(data => {
+      console.log("final result = ",data);
       done();
     });
   });
-
-
 });
